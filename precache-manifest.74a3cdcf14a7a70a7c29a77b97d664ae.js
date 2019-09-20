48e930e17a7cc64060458e492a6e77bc2395e14f
vibecode <rumashkin@gmail.com>
2019-09-20 16:05:15 +0200

    Updates

---
+++ precache-manifest.74a3cdcf14a7a70a7c29a77b97d664ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f74d2b18567b21003fe9283dcf8fe65",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "af04ec3db053e7a7a414",
+    "url": "/snake-game/static/css/main.b7d89081.chunk.css"
+  },
+  {
+    "revision": "bc04ac113e045fafe6f7",
+    "url": "/snake-game/static/js/2.f91f8c78.chunk.js"
+  },
+  {
+    "revision": "af04ec3db053e7a7a414",
+    "url": "/snake-game/static/js/main.7e19fd51.chunk.js"
+  },
+  {
+    "revision": "a50e251ebc82741c7d45",
+    "url": "/snake-game/static/js/runtime~main.0da671ed.js"
+  },
+  {
+    "revision": "9838ea6b336247d55c55cae8b992b527",
+    "url": "/snake-game/static/media/Apple.9838ea6b.svg"
+  },
+  {
+    "revision": "cb68bc3adfa37ec6db9fe8f0231c12e1",
+    "url": "/snake-game/static/media/Keys.cb68bc3a.svg"
+  }
+]);
